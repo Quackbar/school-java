baeea324c7bd83d172788c9fc28f4df2c309563c
Bernard Kintzing <bernardkintzing@gmail.com>
2017-04-18 21:56:14 -0600

    meh

--- Bernard/Sem6/Personal/src/Run.java
+++ Bernard/Sem6/Personal/src/Run.java
@@ -4,14 +4,19 @@
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Run extends JFrame implements KeyListener{
 
     gamePanel game = new gamePanel();
 
+    Timer timer = new Timer();
+
     public Run(){
         this.add(game);
         addKeyListener(this);
+        startTimer();
     }
 
     public void keyReleased(KeyEvent e){}
@@ -20,7 +25,7 @@ public class Run extends JFrame implements KeyListener{
         if((e.getKeyCode() == 87) || (e.getKeyCode() == 38)){ //W and Up
             if(!(game.charY == 0)){
                 game.charY -= 10;
-                if(game.occupied[game.charX /10][game.charY / 10].equals(null))
+                if(!game.occupied[game.charX /10][game.charY / 10].equals("empty"))
                     game.charY +=10;
             }
             game.repaint();
@@ -28,7 +33,7 @@ public class Run extends JFrame implements KeyListener{
         else if((e.getKeyCode() == 65) || (e.getKeyCode() == 37)){ //A and Left
             if(!(game.charX == 0)){
                 game.charX -= 10;
-                if(game.occupied[game.charX /10][game.charY / 10].equals(null))
+                if(!game.occupied[game.charX /10][game.charY / 10].equals("empty"))
                     game.charX +=10;
             }
             game.repaint();
@@ -36,7 +41,7 @@ public class Run extends JFrame implements KeyListener{
         else if((e.getKeyCode() == 83) || (e.getKeyCode() == 40)){ //S and Down
             if(!(game.charY == 670)){
                 game.charY += 10;
-                if(game.occupied[game.charX /10][game.charY / 10].equals(null))
+                if(!game.occupied[game.charX /10][game.charY / 10].equals("empty"))
                     game.charY -=10;
             }
             game.repaint();
@@ -44,14 +49,14 @@ public class Run extends JFrame implements KeyListener{
         else if((e.getKeyCode() == 68) || (e.getKeyCode() == 39)){ //D and Right
             if(!(game.charX == 1510)){
                 game.charX += 10;
-                if(game.occupied[game.charX /10][game.charY / 10].equals(null))
+                if(!game.occupied[game.charX /10][game.charY / 10].equals("empty"))
                     game.charX -=10;
             }
             game.repaint();
         }
         else if(e.getKeyCode() == 90){ //Z Wall
             if(!(game.walls == 0)){
-                if(game.wallX.size() == 0){
+                if(game.wallX.size() == 0){ //No walls placed yet
                     game.occupied[game.charX /10][game.charY / 10] = "wall";
                     createWall();
                 }
@@ -72,7 +77,8 @@ public class Run extends JFrame implements KeyListener{
         }
         else if(e.getKeyCode() == 88){ //X Door
             if(!(game.doors == 0)){
-                if(game.doorX.size() == 0){
+                if(game.doorX.size() == 0){ //No doors placed yet
+                    game.occupied[game.charX /10][game.charY / 10] = "door";
                     createDoor();
                 }
                 else{
@@ -84,6 +90,7 @@ public class Run extends JFrame implements KeyListener{
                         }
                     }
                     if(!filled){
+                        game.occupied[game.charX /10][game.charY / 10] = "door";
                         createDoor();
                     }
                 }
@@ -92,19 +99,19 @@ public class Run extends JFrame implements KeyListener{
     }
 
     public boolean checkOccupied(int i){
-        if((i >= game.doorX.size()) || (game.doorX.size() == 0)){
+        if((i >= game.doorX.size()) || (game.doorX.size() == 0)){ //i is out of bounds for doors
             if((game.wallX.get(i) == game.charX) && (game.wallY.get(i) == game.charY))
                 return false;
             else
                 return true;
         }
-        else if((i >= game.wallX.size()) || (game.wallX.size() == 0)){
+        else if((i >= game.wallX.size()) || (game.wallX.size() == 0)){ //i is out of bounds for walls
             if((game.doorX.get(i) == game.charX) && (game.doorY.get(i) == game.charY))
                 return false;
             else
                 return true;
         }
-        else{
+        else{ // i is not out of bounds for wall or door
             if((game.doorX.get(i) == game.charX) && (game.doorY.get(i) == game.charY))
                 return false;
             else if((game.wallX.get(i) == game.charX) && (game.wallY.get(i) == game.charY))
@@ -129,7 +136,14 @@ public class Run extends JFrame implements KeyListener{
         game.stats.setText("   Health: " + game.health + "     Walls(Z): " + game.walls + "    Doors(X): " + game.doors + "    Day: " + game.days + "    Time: " + game.day);
         repaint();
     }
-
+    public void startTimer(){
+        timer.scheduleAtFixedRate(new TimerTask(){
+            public void run(){
+                game.repaint();
+                game.goombaRepaint = true;
+            }
+        }, 0, 100);
+    }
     public static void main(String[]  args){
         Run m = new Run();
         m.setVisible(true);
--- Bernard/Sem6/Personal/src/gamePanel.java
+++ Bernard/Sem6/Personal/src/gamePanel.java
@@ -8,9 +8,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class gamePanel extends JPanel{
 
@@ -19,13 +18,9 @@ public class gamePanel extends JPanel{
     boolean day = true;
     JLabel stats = new JLabel("   Health: " + health + "     Walls(Z): " + walls + "    Doors(X): " + doors + "    Day: " + days + "    Time: " + day + " 0:00:30");
 
-    //Day timer
-    Timer time = new Timer();
-    int length = 10, delay = 1000, period = 1000;
-
     //Pictures
     BufferedImage grid, character;
-    BufferedImage[] goomba;
+    BufferedImage[] goomba = new BufferedImage[5];
 
     //Locations (Grid is 0,0 to 1510,670
     int charX = 700, charY = 400;
@@ -36,13 +31,24 @@ public class gamePanel extends JPanel{
     Integer[] goombaX = new Integer[5];
     int[] goombaY = {0, 0, 0, 0, 0};
 
+    //2D array of whats in every coordinate
     String[][] occupied = new String[151][67];
+
+    //What the goomba's need
+    Random random = new Random();
+    boolean direction, positive, empty, goombaRepaint;
 
     public gamePanel(){
         this.add(stats);
-        try{
+        try{ //Grid
             grid = ImageIO.read(new File("./Pictures/grid.png"));
+        } catch(Exception e){System.out.println("Could not create grid: " + e.getStackTrace());}
+
+        try{ //Character
             character = ImageIO.read(new File("./Pictures/character.png"));
+        } catch(Exception e){System.out.println("Could not create character: " + e.getStackTrace());}
+
+        try{ //Goomba
             for(int i = 0; i < goomba.length; i ++){
                 goomba[i] = ImageIO.read(new File("./Pictures/goomba" + i +".png"));
                 int randNum = ThreadLocalRandom.current().nextInt(0, 151 + 1);
@@ -51,48 +57,68 @@ public class gamePanel extends JPanel{
                 }
                 goombaX[i] = randNum * 10;
             }
-        }
-        catch(Exception e){}
+        } catch(Exception e){System.out.println("Could not create goomba: " + e.getStackTrace());}
+
         for(int row = 0; row < 151; row++){
             for(int col = 0; col < 67; col++){
-                occupied[row][col] = null;
+                occupied[row][col] = "empty";
             }
         }
-        startTimer();
     }
 
+    //Checks if an int is present in an array of Integers
     public static boolean contains(Integer[] arr, Integer item) {
         return Arrays.stream(arr).anyMatch(item::equals);
     }
 
-    public void startTimer(){
-        time.scheduleAtFixedRate(new TimerTask(){
-            @Override
-            public void run(){
-                changeDay();
+    public void createGoombaLocation(int i){
+        empty = false;
+        while(!empty){
+            direction = random.nextBoolean(); //true = left-right, false = up-down
+            positive = random.nextBoolean(); // true = right-down, false = left-up
+            if(direction){ //left-right
+                if(positive) //right
+                    goombaX[i] += 10;
+                else //left
+                    goombaX[i] -= 10;
+            } else{ //up-down
+                if(positive) //down
+                    goombaY[i] += 10;
+                else //up
+                    goombaY[i] -= 10;
             }
-        },delay, period);
-    }
-
-    public void changeDay(){
-        length -= 1;
-        if(length == 0){
-            time.cancel();
-            startTimer();
+            if((goombaX[i] < 0) || (goombaX[i] > 1500) || (goombaY[i] < 0) || goombaY[i] > 660) //Check if goomba is out of bounds
+                empty = false;
+            else{
+                if(occupied[goombaX[i] / 10][goombaY[i] / 10].equals("empty"))
+                    empty = true;
+            }
         }
     }
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.drawImage(grid, 200, 200, null);
-        for(int i = 0; i < wallX.size(); i++){
+        for(int i = 0; i < wallX.size(); i++){ //Draw walls
             g.setColor(Color.BLACK);
             g.fillRect(wallX.get(i) + 200, wallY.get(i) + 200, 11, 11);
         }
-        for(int i = 0; i < doorX.size(); i ++){
+        for(int i = 0; i < doorX.size(); i ++){ //Draw doors
             g.setColor(Color.GRAY);
             g.fillRect(doorX.get(i) + 200, doorY.get(i) + 200, 11, 11);
         }
-        g.drawImage(character, charX + 201, charY + 201, null);
+        g.drawImage(character, charX + 201, charY + 201, null); //Draw character
+        if(goombaRepaint){
+            for(int i = 0; i < goomba.length; i++){ //Draw gooombas moving
+                createGoombaLocation(i);
+                g.drawImage(goomba[i], goombaX[i] + 201, goombaY[i] + 201, null);
+            }
+            goombaRepaint = false;
+        }
+        else{
+            for(int i = 0; i < goomba.length; i++){ //Draw gooombas still
+                g.drawImage(goomba[i], goombaX[i] + 201, goombaY[i] + 201, null);
+            }
+        }
     }
 }
